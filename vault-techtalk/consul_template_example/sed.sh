3bbcc21d45838dec86390ee8005d4189291e7f84
Ihar Ratner <igo.rat@outlook.com>
2019-10-01 18:09:35 +0300

    Update sed.sh

--- vault-techtalk/consul_template_example/sed.sh
+++ vault-techtalk/consul_template_example/sed.sh
@@ -3,4 +3,5 @@ X=`cat /home/vagrant/vault/.vault-token`
 echo $X
 sed -i "/token / s/="[^"][^"]*"/=\"$X\"/" config.hcl
 echo $VAULT_ADDR
-sed -i "/address / s/="[^"][^"]*"/=\"http:\/\/$VAULT_ADDR\"/" config.hcl
+#sed -i "11s#.*# s#="[^"][^"]*"#=\"$VAULT_ADDR\"#" config.hcl
+sed -i "11s#.*# address = \"$VAULT_ADDR\"#" config.hcl
